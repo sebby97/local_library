c24f50f515825ed8ac7f4981f7109fc588a45b99
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro.local>
2017-11-08 23:48:39 -0500

    hw9

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
